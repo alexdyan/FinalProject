b29ae4d40da344e3fde93fe5feb7c010c7c9739d
alexdyan <33070722+alexdyan@users.noreply.github.com>
2017-11-16 10:41:13 -0500

    serial talks to processing

--- FinalProject.ino
+++ FinalProject.ino
@@ -1,11 +1,39 @@
-int ledPin = 10;
+#include "BetterButton.h"
+
+int pad1Pin = A17;
+int pad2Pin = A16;
+int pad3Pin = A15;
+int pad4Pin = A14;
+
+BetterButton p1 (pad1Pin, 0);
+BetterButton p2 (pad2Pin, 0);
+BetterButton p3 (pad3Pin, 0);
+BetterButton p4 (pad4Pin, 0);
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  p1.pressHandler(onPress);
+  p1.releaseHandler(onRelease);
+  p2.pressHandler(onPress);
+  p2.releaseHandler(onRelease);
+  p3.pressHandler(onPress);
+  p3.releaseHandler(onRelease);
+  p4.pressHandler(onPress);
+  p4.releaseHandler(onRelease);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  p1.process();
+  p2.process();
+  p3.process();
+  p4.process();
 }
+
+void onPress(int buttonNum) {
+  Serial.write(0);
+}
+
+void onRelease(int buttonNum) {
+  Serial.write(1);
+}
+
